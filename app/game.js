8c94158f2e953f791c23c46887ffa72d0328b7d5
John Preston <wcerfgba@riseup.net>
2016-04-24 18:12:41 +0100

    Hook-up socket.io.

--- app/game.js
+++ app/game.js
@@ -1,12 +1,25 @@
 "use strict";
 
 var elements = require("elements");
+var socket = io();
 
 exports = module.exports = {
     start: function () {
         elements.landing.hide();
         elements.canvas.fillInner();
 
-        console.log(elements.name.value());
+        // Send name to server.
+        socket.emit("new_player", { name: elements.name.value() });
+
+        // Setup game.
+        socket.on("game_data", function (data) {
+            console.log(data.id);
+        });
     }
+}
+
+function Game(gameState) {
+    this.players = gameState.players;
+    this.playerPositions = gameState.playerPositions;
+    this.balls = gameState.balls;
 }
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require("express");
+var http = require("http");
+var socketio = require("socket.io");
+
 var app = express();
+var server = http.Server(app);
+var io = socketio(server);
+
+server.listen(3000);
+console.log("Listening on port 3000...");
 
 app.use(express.static("public"));
 
-app.listen(3000, function () {
-    console.log("Listening on port 3000");
+io.on("connection", function (socket) {
+    console.log("Connection received: ", socket.id);
+
+    socket.on("new_player", function (data) {
+        console.log("New player: ", data.name);
+
+        socket.emit("game_data", { id: socket.id });
+    });
 });
