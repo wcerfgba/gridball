0b21a9bd27407f69ad3a6865fd94a923c32dd0a9
John Preston <wcerfgba@riseup.net>
2016-04-28 17:24:50 +0100

    Rescale for Y-axis trigonometry.

--- common/magic.js
+++ common/magic.js
@@ -11,34 +11,59 @@ var maxPlayers = 919;
 module.exports.maxShells = maxShells;
 module.exports.maxPlayers = maxPlayers;
 
+/* Players have a shield of a defined width in radians. */
+var halfShieldWidth = Math.PI / 6;
+module.exports.halfShieldWidth = halfShieldWidth;
+
 /* Various lengths affect both the gameplay and the implementation of the 
  * simulation: the radius of the ball, the radius of the player node, the 
  * radius of the player shield, and the distance between players on the grid 
- * (which in turn determines the distances to zone boundaries). We will assume 
- * that the ratios:
- *     ball radius : player radius : shield radius : player distance
- * are equal to 1 : 3 : 3.5 : 18. In a fully-enclosed zone, the maximum distance
- * between the closest point on the ball and the player is given by:
- *     0.5 * player distance - 2 * ball radius - player radius
- * which in this instance is 0.5 * 18 - 2 * 1 - 3 = 4. For performance reasons 
- * we would like to work only in integer co-ordinates, and a granularity of only 
- * four to five distances when the ball is approaching the player is clearly 
- * insufficient. At the same time, the maximum width of the grid is given by:
+ * (which in turn determines the distances to zone boundaries). For safety and 
+ * efficiency, we would like to store as many values as possible as integers. 
+ * If these lengths can be represented as ratios then we can scale them up 
+ * appropriately to derive integer values, at a precision suitable for the 
+ * simulation.
+ *
+ * Because the hexagonal player grid is placed over a Cartesian grid, the 
+ * increments for the X- and Y-axes between cells is not equal. The grid has 
+ * an edge at the top and bottom, and so the cells which tesselate into this 
+ * grid are hexagons with edges at the sides. The X-increment between adjacent 
+ * cells is the 'distance between players' that we have already mentioned. When 
+ * moving up or down, we move half player distance along the X-axis, and move 
+ * along the Y-axis by player distance * cos(pi / 6). This ratio is roughly 
+ * 0.866, so providing player distance is a multiple of 500, the Y-increment 
+ * will be an integer.
+ *
+ * Thus the ratios for consideration are:
+ *     ball radius : player radius : shield radius : 
+ *     half player distance : Y increment
+ *
+ * which we will assume are equal to 1 : 3 : 3.5 : 10 : 2 * 10 * 0.866. Scaling 
+ * by 50 gives 50 : 150 : 175 : 500 : 866.
+ *
+ * In a fully-enclosed zone, the maximum distance between the closest point on 
+ * the ball and the player is given by:
+ *     half player distance - 2 * ball radius - player radius
+ * which in this instance is 500 - 2 * 50 - 150 = 250. At the same time, the 
+ * maximum width of the grid is given by:
  *     (2 * side length - 1) * player distance
- * which with these ratios evaluates to (2 * 18 - 1) * 18 = 630. If we choose a 
- * multiplier of 100, then we get a total width of 63000, which still fits into 
- * an unsigned short, and a minimum zone edge ball distance of 400. */
-var ballRadius = 100;
+ * which with these ratios evaluates to (2 * 18 - 1) * 1000 = 35000. The the 
+ * calculated values give good resolution for the ball positions, and the 
+ * largest expected values for co-ordinates fit nicely into an unsigned short.
+ */
+var ballRadius = 50;
 var ballDiameter = 2 * ballRadius;
-var playerRadius = 300;
-var shieldRadius = 350;
-var halfShieldWidth = Math.PI / 6;
-var playerDistance = 1800;
+var playerRadius = 150;
+var shieldRadius = 175;
+var halfPlayerDistance = 500;
+var yIncrement = 866;
+var playerDistance = 2 * halfPlayerDistance;
 module.exports.ballRadius = ballRadius;
 module.exports.ballDiameter = ballDiameter;
 module.exports.playerRadius = playerRadius;
 module.exports.shieldRadius = shieldRadius;
-module.exports.halfShieldWidth = halfShieldWidth;
+module.exports.halfPlayerDistance = halfPlayerDistance;
+module.exports.yIncrement = yIncrement;
 module.exports.playerDistance = playerDistance;
 
 /* The time taken for a ball to collide with a player from the minimum zone 
@@ -46,18 +71,16 @@ module.exports.playerDistance = playerDistance;
  *     distance / (tick rate * speed)
  * where the tick rate is the number of times the simulation is updated per 
  * second. If we operate at 20 ticks per second, this gives a maximum time of 
- * 400 / (20 * 1) = 20 seconds, which is very slow. Speed 8 gives 2.5 seconds, 
- * which is a more acceptable minimum. The time difference between speed 8 and 
- * speed 9 is 0.27 seconds, which is a 10.8% increase in speed. This is the 
+ * 250 / (20 * 1) = 12.5 seconds, which is very slow. Speed 5 gives 2.5 seconds, 
+ * which is a more acceptable minimum. The time difference between speed 5 and 
+ * speed 6 is 0.41666 seconds, which is a 16.66% increase in speed. This is the 
  * largest increase in the speed series, and so the discretization of the speed 
  * makes an interesting gameplay mechanic: slow balls gain speed quicker than 
- * faster balls. At the other end of the scale, speed 200 gives 0.1 seconds, 
- * which can be a maximum, with an increase from speed 199 of about half a 
- * millisecond.
- *  */
+ * faster balls. At the other end of the scale, speed 125 gives 0.1 seconds, 
+ * which can be a maximum, with an increase from speed 124 of about 0.8ms. */
 var tickRate = 20;
-var minBallSpeed = 8;
-var maxBallSpeed = 200;
+var minBallSpeed = 5;
+var maxBallSpeed = 125;
 module.exports.tickRate = tickRate;
 module.exports.minBallSpeed = minBallSpeed;
 module.exports.maxBallSpeed = maxBallSpeed;
@@ -100,9 +123,7 @@ module.exports.hexArray = hexArray;
  * that row. The middle row has an offset of playerDistance / 2, and each row 
  * out from the center adds on an extra playerDistance / 2 each time. We 
  * precompute these values here for efficiency. */
-var halfPlayerDistance = playerDistance / 2;
 var rowOffsets = [ ];
-
 for (var i = maxShells; 0 < i; i--) {
     rowOffsets.push(halfPlayerDistance * (i + 1));
 }
@@ -110,21 +131,34 @@ rowOffsets.push(halfPlayerDistance);
 for (var i = 1; i < maxShells + 1; i++) {
     rowOffsets.push(halfPlayerDistance * (i + 1));
 }
-module.exports.halfPlayerDistance = halfPlayerDistance;
 module.exports.rowOffsets = rowOffsets;
+
+/* It is also useful to have the relative co-ordinates of the points on the 
+ * zone hexagon from the center of the player. This is used in rendering and 
+ * collision detection. zonePoints and hexVectors are in one-to-one 
+ * correspondence and each zone point is the anticlockwise point of its 
+ * vector. */
+var zoneRadius = playerDistance / Math.sqrt(3);
+var zonePoints = [ ];
+for (var i = 0; i < 6; i++) {
+    zonePoints.push({ x: zoneRadius *  Math.sin(i * Math.PI / 3),
+                      y: zoneRadius * -Math.cos(i * Math.PI / 3) });
+}
+module.exports.zoneRadius = zoneRadius;
+module.exports.zonePoints = zonePoints;
 
 /* Takes an array of indices [ row, cell ] referencing a cell in a hex array, 
  * and computes the position of the center of that cell. */
 function cellToPosition(cell) {
     return { x: rowOffsets[cell[0]] + cell[1] * playerDistance,
-             y: halfPlayerDistance + cell[0] * playerDistance }; 
+             y: zoneRadius + cell[0] * yIncrement }; 
 };
 module.exports.cellToPosition = cellToPosition;
 
 /* Takes a position object with co-ordinates (x, y) as properties, and computes 
  * the indices for that position in a hex cell array. */
 function positionToCell(position) {
-    var row = Math.floor(position.y / playerDistance);
+    var row = Math.floor((position.y - zoneRadius) / yIncrement);
     return [ row,
              Math.floor((position.x - rowOffsets[row] + halfPlayerDistance) / 
                 playerDistance) ];
@@ -166,30 +200,6 @@ function neighbourCell(cell, direction) {
     }
 }
 module.exports.neighbourCell = neighbourCell;
-
-/* It is useful to have an array of vectors describing a hexagon of cells on 
- * the grid. If a cell vector is added componentwise to each hexVector, the 
- * resulting vectors are for the neighbours of the original cell. If each 
- * hexVector is added componentwise in turn, then the sequence describes a 
- * hexagonal walk on the grid, ending on the original vector, in the top-right 
- * of the walked hexagon. 
-var hexVectors = [ [  1,  1 ], [  1, 0 ], [ 0, -1 ],
-                   [ -1, -1 ], [ -1, 0 ], [ 0,  1 ] ];
-module.exports.hexVectors = hexVectors;
-*/
-/* It is also useful to have the relative co-ordinates of the points on the 
- * zone hexagon from the center of the player. This is used in rendering and 
- * collision detection. zonePoints and hexVectors are in one-to-one 
- * correspondence and each zone point is the anticlockwise point of its 
- * vector. */
-var zoneRadius = playerDistance / Math.sqrt(3);
-var zonePoints = [ ];
-for (var i = 0; i < 6; i++) {
-    zonePoints.push({ x: zoneRadius *  Math.sin(i * Math.PI / 3),
-                      y: zoneRadius * -Math.cos(i * Math.PI / 3) });
-}
-module.exports.zoneRadius = zoneRadius;
-module.exports.zonePoints = zonePoints;
 
 /* playerPositions holds an array of grid vectors that map each successive 
  * player to their indices in a hexArray of size maxShells. It uses the 
