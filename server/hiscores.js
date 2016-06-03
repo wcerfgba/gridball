5353ae1bd2fd6d0e9e24ce0a849d856a958ab99e
John Preston <wcerfgba@riseup.net>
2016-06-03 22:39:55 +0100

    Migrate high scores to redis.

--- server/hiscores.js
+++ server/hiscores.js
@@ -1,47 +1,19 @@
 "use strict";
 
+var redis = require("redis");
 var fs = require("fs");
 var m = require("../common/magic");
 
-var filename = "hiscores.txt";
-var htmlFilename = "public/hiscores.html";
 var maxScores = 20;
+var htmlFilename = "public/hiscores.html";
 
-var scores = [ ];
-function scoreSort(a, b) {
-    return b.score - a.score;
-}
+var client = redis.createClient(process.env.REDIS_URL || "");
+client.on("error", function (err) {
+    console.log(err);
+});
 
-function readScores() {
-    try {
-        scores = JSON.parse(fs.readFileSync(filename));
-    } catch (e) {
-        if (e.code !== "ENOENT") { throw e; }
-    }
-    return scores;
-}
-module.exports.readScores = readScores;
-
-function renderScores() {
-    var html = '<h2 id="scores-title">High Scores</h2><ul id="score-list">';
-    var itemStart = '<li><span class="score-name">';
-    var itemMiddle = '</span><span class="score-score">';
-    var itemEnd = '</li>'
-
-    for (var i = 0; i < scores.length; i++) {
-        html += itemStart + scores[i].name + itemMiddle +
-                            scores[i].score + itemEnd;
-    }
-
-    html += '</ul>';
-
-    fs.writeFileSync(htmlFilename, html);
-}
-    
 function updateScores(state, tickNum) {
-    readScores();
-
-    var changed = false;
+    var currentScores = [ ];
 
     for (var i = 0; i < state.players.length; i++) {
         for (var j = 0; j < state.players[i].length; j++) {
@@ -49,41 +21,41 @@ function updateScores(state, tickNum) {
             if (!player || player.health === 0) { continue; }
             var score = Math.round((tickNum - player.joinTick) / 
                                    (1000 / m.tickTime));
-
-            var position = scores.length;
-            while (position > 0) {
-                if (scores[position] &&
-                    scores[position].name === player.name) {
-                    break;
-                } else if (scores[position - 1].score <= score) {
-                    position--;
-                } else {
-                    break;
-                }
-            }
-            if (position < maxScores - 1) {
-                if (scores[position] &&
-                    scores[position].name === player.name &&
-                    scores[position].score < score) {
-                        scores[position].score = score;
-                        scores.sort(scoreSort);
-                } else {
-                    scores.splice(position, 0, { name: player.name,
-                                                 score: score });
-                    scores.splice(maxScores);
-                }
-                changed = true;
-            }
+            currentScores.push(score, player.name);
         }
     }
 
-    if (changed) {
-        fs.writeFileSync(filename, JSON.stringify(scores));
-        renderScores();
-    }
-
-    readScores();
+    client.multi()
+          .del("current")
+          .zadd("current", currentScores)
+          .zunionstore("hiscores", 2, "hiscores", "current",
+                       "aggregate", "max")
+          .zremrangebyrank("hiscores", 0, -maxScores)
+          .exec();
+    client.zrevrange("hiscores", 0, maxScores, "withscores",
+                     function (err, res) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            renderScores(res);
+                        }
+                     });
 }
 module.exports.updateScores = updateScores;
+
+function renderScores(scores) {
+    var html = '<h2 id="scores-title">High Scores</h2><ul id="score-list">';
+    var itemStart = '<li><span class="score-name">';
+    var itemMiddle = '</span><span class="score-score">';
+    var itemEnd = '</li>'
+
+    for (var i = 0; i < scores.length - 1; i += 2) {
+        html += itemStart + scores[i] + itemMiddle + scores[i + 1] + itemEnd;
+    }
+
+    html += '</ul>';
+
+    fs.writeFileSync(htmlFilename, html);
+}
 
 exports = module.exports;
